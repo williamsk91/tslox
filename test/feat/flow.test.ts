bae5d4bd252142027c1116131a49cc6b4ff6abee
williams <williamskusnandi@gmail.com>
2021-09-17 05:13:18 +1000

    docs: lox differences readme

--- test/feat/flow.test.ts
+++ test/feat/flow.test.ts
@@ -12,4 +12,12 @@ describe("flow", () => {
     );
     expect(logs).toStrictEqual(["true"]);
   });
+  test("ternary", () => {
+    const logs = runLoxTest(
+      `
+        print 5 > 3 ? "yes" : "no" ;
+      `
+    );
+    expect(logs).toStrictEqual(["yes"]);
+  });
 });
